2100d5c78e0b809600ab1d3d0b0c7a63badaa93f
Brandon Zimlich-Vining <branon.zimlich-vining@threatq.com>
2024-12-09 14:36:42 -0600

    Remove token from code, add in extra messages for rate limiting

--- bolt-app/app.py
+++ bolt-app/app.py
@@ -13,11 +13,13 @@ logging.basicConfig(
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+VT_TOKEN = os.environ.get("VT_TOKEN")
 
 @app.message()
 def parse_message(logger, message, say):
     logger.info(message)
     ips = ip_helpers.parse_for_ip(message['text'])
+    # TODO Remove duplicate IPs
     ip_count = len(ips)
     if ip_count == 0:
         logger.info("No IP addresses found in the message")
@@ -25,12 +27,12 @@ def parse_message(logger, message, say):
         # parsed_vt_data = []
         if ip_count == 1:
             say(f"{ip_count} IP Address was found in the message")
-        else
+        else:
             say(f"{ip_count} IP Addresses were found in the message")
             say(blocks=[{"type": "divider"}])
             for ip in ips:
                 logger.info(f"IP address found: {ip}")
-                vt_data = ip_helpers.enrich_virustotal(logger, ip)
+                vt_data = ip_helpers.enrich_virustotal(VT_TOKEN, logger, say, ip)
                 parsed_vt_data = ip_helpers.parse_vt_data(vt_data)
                 say(blocks=[ip_helpers.build_block_response(ip, parsed_vt_data)])
                 say(blocks=[{"type": "divider"}])
